1800a46155f32a03d19540642d0838688f5e714e
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-11-24 23:04:58 +0100

    three and three + pair added

--- script.js
+++ script.js
@@ -45,10 +45,16 @@ function hand() {
         rank.sort(function(a, b) {
             return a - b;
         });
-    
-
-        if(rank.every(e => e >= 9) && suit.every(e => e == suit[0])){
-            console.log("Royal Flush");
+       
+        //Royal Flush
+        if(suit.every(e => e == suit[0])){
+            if(rank.every(e => e >= 9)) {
+                console.log("Royal Flush");
+            } else {
+        // Flush
+                console.log("Flush");
+            }
+            
         // Straight Flush: the cards are in order by rank. So the difference between the max and min should always be 4 if all the cards are different in rank
         } else if(Math.max.apply(Math, rank) - Math.min.apply(Math, rank) == 4) {
             if(suit.every(e => e == suit[0])){
@@ -63,6 +69,7 @@ function hand() {
                 } else {
                     return false;
                 } 
+        // Straight
             } else {
                 console.log("Straight");
             }
@@ -78,14 +85,38 @@ function hand() {
             for(let i=0; i<rank.length; i++) {
                 if(rank.filter(e => e !== rank[i]).length == 1) {   
                     return console.log("four");;
+                } else if (rank.filter(e => e !== rank[i]).length == 2){
+                    let pairArray = rank.filter(e => e !== rank[i]);
+                    console.log(pairArray);
+                    for(let i=0; i<rank.length; i++) {
+                        if(pairArray[i] !== pairArray[i+1]) {
+                            return console.log("Three");
+                        } else {
+                            return console.log("Three + pair")
+                        }
+                    }
                 }
             }
             
         }
+
+        else if (rank.filter(e => e == rank[0]).length == 3) {
+           console.log("ok");
+            /*
+            let pairArray = rank.filter(e => e !== rank[0]);
+            for(let i=0; i<rank.length; i++) {
+                if(pairArray[i] !== pairArray[i+1]) {
+                    console.log("Three");
+                } else {
+                    console.log("Three + pair")
+                }
+            }*/
+        }
 }
 
 
-hand("2C", "3D", "4C", "5S","6C")
+hand("9C", "JC", "QC", "KC","AC")
 
 hand("8C", "8D", "8C", "8S","13C")
 
+hand("3C", "3   D", "2C", "2S","2C")
