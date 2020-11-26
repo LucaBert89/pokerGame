13d39694fb817e97ded23577a7f379a16d77f2a6
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-11-26 16:37:51 +0100

    straight, royal and flush added

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function hand() {
             };
   
         
-
         
     // converting letter cards to number
             switch(rank[i]){
@@ -46,86 +45,89 @@ function hand() {
             return a - b;
         });
        
+        
+        // Straight Flush: the cards are in order by rank. So the difference between the max and min should always be 4 if all the cards are different in rank
+        if (Math.max.apply(Math, rank) - Math.min.apply(Math, rank) == 4) {
+        
+        for(i=0; i<rank.length; i++) {
+            if(rank[i+1] - rank[i] !== 1) {
+                break;
+            } 
+        }
         //Royal Flush
-        if(suit.every(e => e == suit[0])){
-            if(rank.every(e => e >= 9)) {
+        if(suit.every(e => e == suit[0])) {
+            if(rank.every(e => e >= 10)) {
                 console.log("Royal Flush");
             } else {
-        // Flush
-                console.log("Flush");
+        // five card of same suit 
+                console.log("Straight Flush")
             }
-            
-        // Straight Flush: the cards are in order by rank. So the difference between the max and min should always be 4 if all the cards are different in rank
-        } else if(Math.max.apply(Math, rank) - Math.min.apply(Math, rank) == 4) {
-            if(suit.every(e => e == suit[0]) && rank.filter(e=>e !== rank[0])){
-                console.log("Straight flush");
-            } else if (rank.filter(e=>e !== rank[0])) {
-                console.log("Straight");
-            } 
-                /*
-                let result = [];
-                for(i=0; i<rank.length; i++){
-                    if(rank[i] == rank[i+1]) {
-                        result.push("not different");
-                    } 
-                }  
-                if(result.length < 1) {
-                    console.log("Straight Flush");
-                } else {
-                    return false;
-                } 
-
-        // Straight correggereeeee
-            } else {
-                for(i=0; i<rank.length; i++){
-                    if(rank[i] == rank[i+1]) {
-                        console.log("Straight");
-                    } 
-                }  
-              
-            }
-            */
-            
-        // five card of same suit  
+        } else {
+        // Straight
+            console.log("Straight");
+        } 
+       
+        // Flush 
         } else if(suit.every(e => e == suit[0])) {
-            console.log("Five cards of the same suit");
+            console.log("Flush");
         }
         
        else if(rank.filter(e => e !== rank[0]).length == 3) {
         let newArray = rank.filter(e => e !== rank[0]);
+    
+        if(newArray.filter(e => e !== newArray[0]).length == 2) {
+            let pairArray = newArray.filter(e => e !== newArray[0]);
+            if(pairArray[0] ==pairArray[1]){
     // two pair
-     if(newArray.filter(e => e !== newArray[0]).length == 1) {
-             console.log("two pair");
+                console.log("two pair");
+            }  else {
     // pair
-         } else if(newArray.filter(e => e !== newArray[0]).length == 2) {
-             console.log("pair")
+                console.log("pair");
+            }
+            
     // three and pair
          } else if(newArray.filter(e => e !== newArray[0]).length == 0) {
-             console.log("three and pair")
+             console.log("Full house")
+    // pair
+         } else if(newArray.filter(e => e !== newArray[0]).length == 1) {
+             console.log("pair");
          }
+        }
       
-     } else if(rank.filter(e => e !== rank[0]).length == 2) {
+      else if(rank.filter(e => e !== rank[0]).length == 2) {
           let newArray = rank.filter(e => e !== rank[0]);
          if(newArray.filter(e => e !== newArray[0]).length == 1) {
-    // three
-             console.log("three");
+    // three of a Kind
+             console.log("three of a Kind");
          } else if(newArray.filter(e => e !== newArray[0]).length == 0) {
     // three and pair
-             console.log("three and pair")
+             console.log("Full house")
          }
     //Four cards of the same rank
      } else if(rank.filter(e => e !== rank[0]).length == 1 || rank.filter(e => e == rank[0]).length == 4) {
-         console.log("four");
+         console.log("four of a kind");
+     } else {
+         console.log("No other hand applies")
      }
-     
-     
+    
 }
 
 
-hand("9C", "JC", "QC", "KC","AC")
 
-hand("7C", "8C", "9C", "JC","QC")
+hand("AC", "JC", "QC", "KC","10C")
 
-hand("1C", "1D", "1C", "2S","2C")
+hand("9C", "10C", "JC", "QC","KC")
 
-hand("2C", "1D", "1C", "1S","1C")
+hand("1C", "1D", "1C", "1S","2C")
+
+hand("2H", "2H", "1C", "1S","1C")
+
+hand("2C", "2D", "3C", "3S","4C")
+
+hand("2C", "3C", "AC", "5C","6C")
+
+hand("2C", "3H", "4C", "5D","6C")
+
+hand("2C", "2H", "2C", "KD","6C")
+
+hand("AC", "AH", "4C", "4D","6C")
