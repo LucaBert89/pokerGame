16e806602bcd03032b68758599616caed78eb7dd
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2021-01-24 16:05:38 +0100

    adding more than 2 player functionality: array of objects for count and array of array for rank

--- script.js
+++ script.js
@@ -1,23 +1,72 @@
-const gameCards = document.querySelectorAll(".player__card");
-const playerActive = [...document.querySelector(".second").children];
 const btnPlay = document.querySelector(".player-active__btn");
 const cardSuit = ["C", "D", "H", "S"];
+const body = document.querySelector("body");
+let player;
+let card;
+
+
+let score = {
+    "highCard":0,
+    "pair": 1,
+    "twopair":2,
+    "threeofaKind":3,
+    "straight":4,
+    "flush":5,
+    "fullHouse":6,
+    "fourofaKind":7,
+    "straightFlush":8,
+    "royalFlush":9,
+}
+
 let randomCard = [];
 let playerCards = [];
 let cpuCards = [];
+let result = [];
 
 let rank = [];
 let suit = [];
 let i = 0;
 
-// assignment of the cards: i looped through the card of the players and assign a random number and suit letter
-let e = true;
-gameCards.forEach(function(element, index) {
+let playerNumbers = prompt("how many players?");
+
+function generatePlayers(playerNumbers) {
+    if(playerNumbers <= 4) {
+        for(j=1; j <= playerNumbers; j++) {
+            player = document.createElement("div");
+
+            player.classList.add(`player`);
+            
+            if(j==1) {
+                player.classList.add("active");
+            } else {
+                player.classList.add(`${j}`);
+            }
+            body.appendChild(player);
+            
+            for(x=1; x <= 5;x++) {
+                card = document.createElement("div");
+                card.classList.add("player__card");
+                player.appendChild(card);
+            }
+        }
+    } else {
+        console.log("no more than 4");
+    }  
+};
+
+generatePlayers(playerNumbers);
+
+function cardGenerator() {
+    const gameCards = document.querySelectorAll(".player__card");
+    // assignment of the cards: i looped through the card of the players and assign a random number and suit letter
+    let e = true;
+
+    gameCards.forEach(function(element, index) {
         // you can't have the same card for two times, so if the current card is already on the table you have to
         // generate another card
         generateCard(element, index); 
         // you can click only on your cards to change them. From 5 start active player cards
-        if(index >= 5) {
+        if(index < 5) {
             gameCards[index].addEventListener("click", function() {
                 // rules of poker: you can't change the card more than two times
                 //if(i < 2) {
@@ -30,17 +79,16 @@ gameCards.forEach(function(element, index) {
                 }  
                 */
             })
-        } 
-
-        btnPlay.addEventListener("click", function() {
-            if(e) {
-                players(randomCard, index);
-                e=false;
-            }
-            
-
-        })     
+        }    
 });
+btnPlay.addEventListener("click", function() {
+        players(randomCard);
+        compare(result)
+}) 
+}
+
+cardGenerator();
+
 
 function generateCard(selectedCard, current) {
     card = (Math.floor(Math.random()*13)+2) + cardSuit[Math.floor(Math.random() * cardSuit.length)];
@@ -57,16 +105,38 @@ function generateCard(selectedCard, current) {
 // separate player cards from cpuCards
 
 function players(random) {
- 
-    
-        playerCards = random.slice(5,10);
-        cpuCards = random.slice(0,5);
-    
+    /*console.log(random.length);
+        playerCards = random.slice(0,5);
+
+        cpuCards = random.slice(5,random.length);
+
     playerCards.forEach(function(element, index) {
         hand(element, index); 
     })
     handCombination(rank, suit);
-}
+    */
+    random.forEach(function(element, index) {   
+    hand(element, index); 
+    })
+
+    const cpuPlayers = [];
+    let cardSplit = Math.ceil(random.length / playerNumbers);
+    console.log(cardSplit);
+    for(let i=0;i < playerNumbers;i++) {
+        cpuPlayers[i] = [];
+        for(let j=0;j<cardSplit;j++) {
+            const value = rank[j+i*cardSplit];
+            if (!value) continue
+           cpuPlayers[i].push(value);
+        }
+        /*cpuCards[i].forEach(function(element, index) {
+            hand(element, index);*/ 
+    };
+        handCombination(cpuPlayers,suit); 
+    }
+    
+
+
 
 
 function replaceCard(current) {
@@ -81,63 +151,75 @@ function replaceCard(current) {
 }
 
 
-function hand(random, selected) {  
+function hand(random) {
         rank.push(random.slice(0,-1));
         suit.push(random[random.length-1]);
-
-}
-
-function handCombination(rank, suit) {
-    var  count = {}; 
-    rank.forEach(function(i) { count[i] = (count[i]||0) + 1;}); 
-    console.log(count);
-    
-    const values = Object.values(count)
-    
-    for (const key of values) {
-        findPair(key, values);
-        threeOrFull(key);
     }
 
-straigth(rank, suit);
-}
-
-function findPair(howmany, array) {
-    //if there are 2 equal cards then it means that it could be a pair or a two pair.
-    if(howmany === 2) {
-        //if the array length is = 4 ex. ["2","1","1","1"] it means there is a pair
-        if(array.length === 4) {
-            console.log("pair");
-        /*if the array length is = 3 that you could have three equal cards and two different
-          ones or two pairs and another one like ex. ["2", "2", "1"] */
-        } else if(values.length === 3) {
-            // so if there aren't no 3 inside the array than it is a two pair for sure
-            if(howmany !== 3) {
-            console.log("twopair");
-            }
-        };
-    }
-}
-
-function threeOrFull(howmany) {
-    if(howmany === 4) {
-        console.log("four of a kind");
-    } else if(howmany === 3) {
-        if(howmany === 2) {
-            console.log("three and pair");
-        } else {
-            console.log("Three of a Kind");
+function handCombination(cpuRank, suit) {
+    var count = {};
+    let totalCount = [];
+    let values;
+    let find;
+    let three;
+    for(let i=0; i<playerNumbers; i++) {
+        count = {};
+        cpuRank[i].forEach(function(e) { 
+            count[e] = (count[e]||0) + 1;
+        });
+        totalCount.push(count);
+        values = Object.values(totalCount[i])
+        
+        find = findPair(values);
+        three = threeOrFull(values);
+        //let strai = straigth(rank);
+        //let flus = flush(suit, sameSuit);
+        if(find == undefined && three == undefined) {
+           high = noOther(); 
         }
     }
+    
+    let sameSuit = [];
 }
 
-function straigth(arrayRank, arraySuit) {
+function noOther () {
+    return result.push(score["highCard"]);
+}
 
+function findPair(howmany) {
+    //if there are 2 equal cards then it means that it could be a pair or a two pair.
+    if(howmany.filter(e => e == 2).length === 1) {
+        //if the array length is = 4 ex. ["2","1","1","1"] it means there is a pair
+            return result.push(score["pair"]);
+            
+            console.log("pair");
+        /*if the array length is = 3 you could have three equal cards and two different
+          ones or two pairs and another one like ex. ["2", "2", "1"] */
+        } else if(howmany.filter(e => e == 2).length === 2) {
+            // so if there aren't no 3 inside the array than it is a two pair for sure
+            return result.push(score["twopair"]);
+        }
+    }
+
+function threeOrFull(howmany) {
+    if(howmany.filter(e => e == 4).length === 1) {
+        return result.push(score["fourofaKind"]);
+    } else if(howmany.filter(e => e == 3).length === 1) {
+        if(howmany.filter(e => e == 2).length === 1) {
+            return result.push(score["fullHouse"]);
+        } else {
+            return result.push(score["threeofaKind"]);
+        }
+    } 
+}
+
+function straigth(arrayRank) {
+   
     arrayRank.sort(function(a, b) {
         return a - b;
     });
     let consecutiveArray = [];
-    let sameSuit = [];
+ 
     arrayRank.forEach(function(element, index){
         if(arrayRank[index+1] - element == 1) {
             consecutiveArray.push(element);
@@ -152,28 +234,41 @@ function straigth(arrayRank, arraySuit) {
 
             if(sameSuit.length == 4) {
                 if(Math.min.apply(Math, consecutiveArray) === 10) {
-                    console.log("Royal Flush");
+                    return result.push(score["royalFlush"]);
                 } else {
-                    console.log("straight flush");
+                    return result.push(score["straightFlush"]);
                 }
             } else {
-                console.log("Straight")
+                return result.push(score["straight"]);
             }
-    } else {
-        arraySuit.forEach(function(element, index){
-            if(element == arraySuit[index+1]){
-                sameSuit.push(element);
-            }
-        })
-
-        if(sameSuit.length == 4) {
-            console.log("Flush");
-        }
-    }
+    }   
+        
 }
 
+function flush(arraySuit, suit) {
+    arraySuit.forEach(function(element, index){
+        if(element == arraySuit[index+1]){
+            suit.push(element);
+        }
+    })
 
+    if(suit.length == 4) {
+        return result.push(score["flush"]);
+    } 
+} 
 
+function compare(r) {
+    if(r.filter(e => e > r[0]).length > 0) {
+        console.log(r);
+        console.log(`you ${player.classList.contains(`player ${r[0]}`)} lose`)
+        console.log("you lose")
+    } else if(r.filter(e => e < r[0]).length > 0){
+        console.log(r);
+        console.log("win");
+    } else {
+        console.log("pair");
+    }
+}
 
  
 /*
